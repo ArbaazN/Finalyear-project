4b69077995b632cc5203e85a719118de311d90c1
ArbaazN <40684834+ArbaazN@users.noreply.github.com>
2022-04-10 12:55:35 +0530

    Update settings.py

--- Finalyear/settings.py
+++ Finalyear/settings.py
@@ -126,14 +126,15 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-heroku config:set DISABLE_COLLECTSTATIC=1
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 #STATIC_URL = '/static/'
-STATIC_ROOT=(os.path.join(BASE_DIR,'static'),)
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
+STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
+STATIC_ROOT=os.path.join(os.path.dirname(BASE_DIR),'static')
 django_heroku.settings(locals())
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
